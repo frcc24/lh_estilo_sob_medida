35ac9fb347b4ef7e7c5de355615d4b741f796156
Franco Rafael <frcc24@users.noreply.github.com>
2023-03-27 20:07:25 -0300

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:estilo_sob_medida/routes/pages.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'core/theme/app_theme.dart';
+import 'firebase_options.dart';
 import 'modules/welcome_screen/welcome_screen.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Style under Mesure',
+      title: 'Style Under Measure',
       getPages: getAppPages(),
       theme: createTheme(),
       themeMode: ThemeMode.light,
